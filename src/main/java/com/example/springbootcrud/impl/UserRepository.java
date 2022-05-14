a97d30807a034df8dcb1d429c32d9fc5832cf2c0
hvaldev <gituser.local@gmail.com>
2022-05-14 06:58:03 -0500

    move-mod

--- src/main/java/com/example/springbootcrud/impl/UserRepository.java
+++ src/main/java/com/example/springbootcrud/impl/UserRepository.java
@@ -7,5 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    public User findByName(String usernames);
+    public User findByName(String username);
 }
